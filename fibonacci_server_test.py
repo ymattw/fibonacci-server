0e34ed7132efefd764faca6761c3dc597f01075e
Matthew Wang <mattwyl@gmail.com>
2016-03-30 21:08:10 +0800

    Do not use assertRegexpMatches (new in python 2.7)

--- fibonacci_server_test.py
+++ fibonacci_server_test.py
@@ -16,32 +16,32 @@ class FibonacciServerTest(unittest.TestCase):
 
     def test_fib_invalid_version(self):
         rv = self._app.get('/v2/fib/0')
-        self.assertRegexpMatches(rv.status, '^501 ')
+        self.assertTrue(rv.status.startswith('501 '))
 
     def test_fib_not_a_number(self):
         rv = self._app.get('/v1/fib/xxx')
-        self.assertRegexpMatches(rv.status, '^400 ')
+        self.assertTrue(rv.status.startswith('400 '))
 
     def test_fib_float_number(self):
         rv = self._app.get('/v1/fib/1.0')
-        self.assertRegexpMatches(rv.status, '^400 ')
+        self.assertTrue(rv.status.startswith('400 '))
 
     def test_fib_negative_numer(self):
         rv = self._app.get('/v1/fib/-1')
-        self.assertRegexpMatches(rv.status, '^400 ')
+        self.assertTrue(rv.status.startswith('400 '))
 
     def test_fib_overflowed_number(self):
         rv = self._app.get('/v1/fib/99999999999999')
-        self.assertRegexpMatches(rv.status, '^413 ')
+        self.assertTrue(rv.status.startswith('413 '))
 
     def test_fib_zero(self):
         rv = self._app.get('/v1/fib/0')
-        self.assertRegexpMatches(rv.status, '^200 ')
+        self.assertTrue(rv.status.startswith('200 '))
         self.assertEqual(rv.data, '[]')
 
     def test_fib_normal(self):
         rv = self._app.get('/v1/fib/2')
-        self.assertRegexpMatches(rv.status, '^200 ')
+        self.assertTrue(rv.status.startswith('200 '))
         self.assertEqual(rv.data, '[0, 1]')
 
 
